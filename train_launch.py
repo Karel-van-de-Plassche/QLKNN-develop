8856b450d02725719a2fecb683ffad8778b036d6
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2017-07-25 18:44:49 +0200

    Added topology scan to train launch

--- train_launch.py
+++ train_launch.py
@@ -1,16 +1,20 @@
 import os
 import sys
 import json
+from itertools import product
 root = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'nns')
 if not os.path.isdir(root):
     os.mkdir(root)
 
 train_dim = 'efeETG_GB'
-l2_scales = [0.0, 0.05, 0.1, 0.2, 0.5, 1, 5, 10]
+#l2_scales = [0.0, 0.05, 0.1, 0.2, 0.5, 1, 5, 10]
+l2_scales = [0.1]
+topologies = [[30, 30, 30], [60, 60], [120]]
 filter = '3'
 
-for l2_scale in l2_scales:
-    name = '_'.join([train_dim, str(l2_scale)])
+for l2_scale, topology in product(l2_scales, topologies):
+    print(topology)
+    name = '_'.join([train_dim, str(l2_scale), str(topology)])
     dir = os.path.join(root, name)
     os.mkdir(dir)
 
@@ -23,5 +27,6 @@ for l2_scale in l2_scales:
         settings = json.load(file_)
         settings['train_dim'] = train_dim
         settings['cost_l2_scale'] = l2_scale
+        settings['hidden_neurons'] = topology
         with open(os.path.join(dir, 'settings.json'), 'w') as file_:
             json.dump(settings, file_, indent=4)
