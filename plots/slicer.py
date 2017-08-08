f13502de5e3d9814a8c15790ccb6316fe3dcac60
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2017-08-08 21:42:04 +0200

    Added threshold plots

--- plots/slicer.py
+++ plots/slicer.py
@@ -23,7 +23,7 @@ from load_data import load_data, load_nn
 #            in_df = in_df.loc[np.isclose(in_df[varname], var, atol=1e-5, rtol=1e-3)]
 #        except KeyError:
 #            pass
-nn_indeces = [46, 58]
+nn_indeces = [46, 58, 60] #nozero <60, zero <60, zero <100
 input, df, __ = load_data(nn_indeces[0])
 df = input.join(df['target'])
 df = df[df['target']<60]
@@ -54,19 +54,55 @@ for index, slice in df.iterrows():
     if np.all(target == 0):
         zero_slices += 1
     else:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
         x = np.linspace(feature.min(),
                         feature.max(),
                         200)
         slice_dict = {name: np.full_like(x, val) for name, val in zip(df.index.names, index)}
         slice_dict[varname] = x
+
+        # Plot target points
         color = target.copy()
         color[target == 0] = 'r'
         color[target != 0] = 'b'
-        plt.scatter(feature, target, c=color)
+        ax.scatter(feature, target, c=color)
+
+        # Plot nn lines
         for nn_index, nn in nns.items():
             nn_pred = nn.get_output(**slice_dict)
-            plt.plot(x, nn_pred, label=nn_index)
-        plt.legend()
+            l = ax.plot(x, nn_pred, label=nn_index)
+            try:
+                thresh = x[nn_pred.index[nn_pred.iloc[:,0] == 0][-1]]
+                ax.axvline(thresh, c=l[0].get_color(), linestyle='dotted')
+                print('network ', nn_index, 'threshold ', thresh)
+            except:
+                print('No threshold for network ', nn_index)
+            else:
+                thresh = None
+
+        # Plot regression
+        try:
+            idx = target.index[target == 0][-1] #index of last zero
+            slope, intercept, r_value, p_value, std_err = stats.linregress(feature[(target.index > idx) & ~target.isnull()], target[(target.index > idx) & ~target.isnull()])
+            thresh_pred = x * slope + intercept
+            thresh_0 = x[thresh_pred < 0][-1]
+
+            ax.plot(x[(thresh_pred > ax.get_ylim()[0]) & (thresh_pred < ax.get_ylim()[1])],
+                    thresh_pred[(thresh_pred > ax.get_ylim()[0]) & (thresh_pred < ax.get_ylim()[1])],
+                    c='black')
+            ax.axvline(thresh_0, c='black', linestyle='dotted')
+        except:
+            print('No threshold')
+        try:
+            idx = target.index[target == 0][-1] #index of last zero
+            print(idx)
+            idx2 = feature[feature > idx][0]
+            print(idx2)
+            ax.axvline(np.mean([idx2, idx]), c='black', linestyle='dashed')
+        except:
+            print('No threshold2')
+        ax.legend()
         plt.show()
     sliced += 1
     if sliced > 1000:
