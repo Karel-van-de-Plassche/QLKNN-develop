14689b4e4ba0e4560ccfc93a865353e21a170f5d
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2017-11-28 20:32:33 +0100

    Added femtoflux, stability_v2 filters. Better debugging

--- training/filtering.py
+++ training/filtering.py
@@ -1,12 +1,13 @@
+from __future__ import division
 import re
 from itertools import product
 from IPython import embed
 import pandas as pd
 import numpy as np
 
-particle_vars = ['pf', 'df', 'vt', 'vr', 'vc']
-heat_vars = ['ef']
-momentum_vars = ['vf']
+particle_vars = [u'pf', u'df', u'vt', u'vr', u'vc']
+heat_vars = [u'ef']
+momentum_vars = [u'vf']
 
 #'vti_GB', 'dfi_GB', 'vci_GB',
 #       'pfi_GB', 'efi_GB',
@@ -14,10 +15,10 @@ momentum_vars = ['vf']
 #       'efe_GB', 'vce_GB', 'pfe_GB',
 #       'vte_GB', 'dfe_GB'
              #'chie', 'ven', 'ver', 'vec']
-def regime_filter(data, gtr, less):
-    bool = pd.Series(np.full(len(data), True), index=data.index)
+def regime_filter(data, leq, less):
+    bool = pd.Series(np.full(len(data), True, dtype='bool'), index=data.index)
     bool &= (data['efe_GB'] < less) & (data['efi_GB'] < less)
-    bool &= (data['efe_GB'] > gtr) & (data['efi_GB'] > gtr)
+    bool &= (data['efe_GB'] >= leq) & (data['efi_GB'] >= leq)
     data = data.loc[bool]
     return data
 
@@ -27,25 +28,33 @@ def stability_filter(data):
         if splitted[0] not in heat_vars + particle_vars + momentum_vars:
             print('skipping {!s}'.format(col))
             continue
-        if splitted[2] in ['TEM', 'ITG'] or (splitted[1] == 'i'):
-            gam_filter = 'ion'
-        elif splitted[2] in ['ETG']:
+        if splitted[2] == 'TEM':
+            gam_filter = 'tem'
+        elif splitted[2] == 'ITG':
+            gam_filter = 'itg'
+        elif splitted[2] == 'ETG':
             gam_filter = 'elec'
+        elif splitted[0] in heat_vars and splitted[1] == 'e':
+            gam_filter = 'multi'
         else:
-            gam_filter = 'both'
+            gam_filter = 'ion'
 
         pre = len(data[col])
         if gam_filter == 'ion':
-            data[col] = data[col].loc[data['gam_less_GB'] != 0]
-        elif gam_filter == 'elec':
             data[col] = data[col].loc[data['gam_leq_GB'] != 0]
-        elif gam_filter == 'both':
-            data[col] = data[col].loc[(data['gam_less_GB'] != 0) | (data['gam_leq_GB'] != 0)]
-        print('{!s} {:.2f}% unstable on {!s} scale'.format(col, np.sum(~data[col].isnull()) / pre * 100, gam_filter))
+        elif gam_filter == 'elec':
+            data[col] = data[col].loc[data['gam_great_GB'] != 0]
+        elif gam_filter == 'multi':
+            data[col] = data[col].loc[(data['gam_leq_GB'] != 0) | (data['gam_great_GB'] != 0)]
+        elif gam_filter == 'tem':
+            data[col] = data[col].loc[data['TEM']]
+        elif gam_filter == 'itg':
+            data[col] = data[col].loc[data['ITG']]
+        print('{:.2f}% of sane {!s:<9} points unstable at {!s:<5} scale'.format(np.sum(~data[col].isnull()) / pre * 100), col, gam_filter)
     return data
 
 def filter_negative(data):
-    bool = pd.Series(np.full(len(data), True), index=data.index)
+    bool = pd.Series(np.full(len(data), True, dtype='bool'), index=data.index)
     for col in data.columns:
         splitted = re.compile('(?=.*)(.)(|ITG|ETG|TEM)_(GB|SI|cm)').split(col)
         if splitted[0] in heat_vars:
@@ -57,8 +66,8 @@ def filter_negative(data):
 def filter_ck(data, bound):
     return (np.abs(data['cki']) < bound) & (np.abs(data['cke']) < bound)
 
-def filter_totsep(data, divsum_factor):
-    bool = pd.Series(np.full(len(data), True), index=data.index)
+def filter_totsep(data, septot_factor):
+    bool = pd.Series(np.full(len(data), True, dtype='bool'), index=data.index)
     for type, spec in product(particle_vars + heat_vars, ['i', 'e']):
         totname = type + spec + '_GB'
         if totname != 'vre_GB' and totname != 'vri_GB':
@@ -69,33 +78,41 @@ def filter_totsep(data, divsum_factor):
             for sep in seps:
                 sepname = type + spec + sep + '_GB'
                 #sepflux += data[sepname]
-                bool &= np.abs(data[sepname]) <= divsum_factor * np.abs(data[totname])
+                bool &= np.abs(data[sepname]) <= septot_factor * np.abs(data[totname])
 
-            print('After filter divsum {!s} {:.2f}% left'.format(totname, 100*np.sum(bool)/startlen))
+            print('After filter {!s:<6} {!s:<6} {:.2f}% left'.format('septot', totname, 100*np.sum(bool)/startlen))
     return bool
 
 def filter_ambipolar(data, bound):
     return (data['absambi'] < bound) & (data['absambi'] > 1/bound)
 
-def sanity_filter(data, ck_bound, divsum_factor, ambi_bound):
+def filter_femtoflux(data, bound):
+    fluxes = [col for col in data if len(re.compile('(?=.*)(.)(|ITG|ETG|TEM)_(GB|SI|cm)').split(col)) > 1 if re.compile('(?=.*)(.)(|ITG|ETG|TEM)_(GB|SI|cm)').split(col)[0] in particle_vars + heat_vars + momentum_vars]
+    absflux = data[fluxes].abs()
+    return ~((absflux < bound) & (absflux != 0)).any(axis=1)
+
+def sanity_filter(data, ck_bound, septot_factor, ambi_bound, femto_bound):
     # Throw away point if negative heat flux
     data = data.loc[filter_negative(data)]
-    print('After filter negative {:.2f}% left'.format(100*len(data)/startlen))
+    print('After filter {!s:<13} {:.2f}% left'.format('negative', 100*len(data)/startlen))
 
 
     # Throw away point if cke or cki too high
     data = data.loc[filter_ck(data, ck_bound)]
-    print('After filter ck {:.2f}% left'.format(100*len(data)/startlen))
+    print('After filter {!s:<13} {:.2f}% left'.format('ck', 100*len(data)/startlen))
 
     # Throw away point if sep flux is way higher than tot flux
-    data = data.loc[filter_totsep(data, divsum_factor)]
-    print('After filter divsum {:.2f}% left'.format(100*len(data)/startlen))
+    data = data.loc[filter_totsep(data, septot_factor)]
+    print('After filter {!s:<13} {:.2f}% left'.format('septot', 100*len(data)/startlen))
 
     data = data.loc[filter_ambipolar(data, ambi_bound)]
-    print('After filter ambipolar {:.2f}% left'.format(100*len(data)/startlen))
+    print('After filter {!s:<13} {:.2f}% left'.format('ambipolar', 100*len(data)/startlen))
+
+    data = data.loc[filter_femtoflux(data, femto_bound)]
+    print('After filter {!s:<13} {:.2f}% left'.format('femtoflux', 100*len(data)/startlen))
 
     # Alternatively:
-    #data = data.loc[filter_negative(data) & filter_ck(data, ck_bound) & filter_totsep(data, divsum_factor)]
+    #data = data.loc[filter_negative(data) & filter_ck(data, ck_bound) & filter_totsep(data, septot_factor)]
 
     return data
     #for col in data.columns:
@@ -110,24 +127,21 @@ def separate_to_store(input, data, name):
     for col in data:
         splitted = re.compile('(?=.*)(.)(|ITG|ETG|TEM)_(GB|SI|cm)').split(col)
         if splitted[0] in heat_vars + particle_vars + momentum_vars + ['gam_leq_GB', 'gam_less_GB']:
-            store[col] = data[col].dropna()
-
-
-
+            store.put(col, data[col].dropna(), format='table')
 
 if __name__ == '__main__':
     dim = 7
 
-    store_name = ''.join([str(dim), 'D_nions0_flat.h5'])
-    store = pd.HDFStore('../' + store_name, 'r')
+    store_name = ''.join(['gen2_', str(dim), 'D_nions0_flat'])
+    store = pd.HDFStore('../' + store_name + '.h5', 'r')
 
     input = store['/megarun1/input']
     data = store['megarun1/flattened']
-    data = data.join(store['megarun1/synthetic'])
-    embed()
 
     startlen = len(data)
     filtered_store = pd.HDFStore('filtered_' + store_name, 'w')
-    data = sanity_filter(data, 50, 1.5, 1.5)
+    data = sanity_filter(data, 50, 1.5, 1.5, 1e-4)
+    data = regime_filter(data, 0, 100)
+    print('After filter {!s:<13} {:.2f}% left'.format('regime', 100*len(data)/startlen))
     data = stability_filter(data)
-    data = regime_filter(data, 0, 60)
+    separate_to_store(input, data, '../filtered_' + store_name + '_filter6')
