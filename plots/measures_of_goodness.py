a1537cc5d004c90f407e024c41fd9d948da41228
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2017-11-30 10:18:28 +0100

    Bugfix! Fixed crash when no combonetworks could be found

--- plots/measures_of_goodness.py
+++ plots/measures_of_goodness.py
@@ -52,18 +52,18 @@ query = (MultiNetwork.select(MultiNetwork.id,
          .join(Postprocess, on=(MultiNetwork.id == Postprocess.multi_network_id))
          .where(MultiNetwork.target_names == Param(target_names))
 )
-results = list(query.dicts())
-df = pd.DataFrame(results)
-df.drop(['id', 'network', 'combo_network'], inplace=True, axis='columns')
-df['multi_network'] = df['multi_network'].apply(lambda el: 'multi_' + str(el))
-df.rename(columns = {'multi_network':'network'}, inplace = True)
-df.set_index('network', inplace=True)
-stats = pd.concat([stats, df])
+if query.count() > 0:
+    results = list(query.dicts())
+    df = pd.DataFrame(results)
+    df.drop(['id', 'network', 'combo_network'], inplace=True, axis='columns')
+    df['multi_network'] = df['multi_network'].apply(lambda el: 'multi_' + str(el))
+    df.rename(columns = {'multi_network':'network'}, inplace = True)
+    df.set_index('network', inplace=True)
+    stats = pd.concat([stats, df])
 
 stats = stats.applymap(np.array)
 #stats[stats.isnull()] = np.NaN
 stats.sort_index(inplace=True)
-embed()
 stats['hidden_neurons_total'] = stats.pop('hidden_neurons').to_frame().applymap(np.sum)
 stats['l2_norm_weighted'] = stats.pop('l2_norm') / stats.pop('hidden_neurons_total')
 stats.dropna(axis='columns', how='all', inplace=True)
@@ -76,14 +76,17 @@ stats.dropna(axis='columns', how='all', inplace=True)
 #print(stats.abs().mean())
 del stats['pop_abs_mis_95width']
 del stats['thresh_rel_mis_95width']
-del stats['dual_thresh_mismatch_95width']
 stats['rms'] = stats.pop('rms')
 stats['thresh'] = stats.pop('thresh_rel_mis_median').abs().apply(np.max)
 stats['no_thresh_frac'] = stats.pop('no_thresh_frac').apply(np.max)
 stats['pop'] = (14 - stats.pop('pop_abs_mis_median').abs()).apply(np.max)
 stats['l2'] = stats.pop('l2_norm_weighted')
 stats['pop_frac'] = (1 - stats.pop('no_pop_frac')).apply(np.max)
-stats['thresh_mismatch'] = stats.pop('dual_thresh_mismatch_median').abs().apply(np.max)
+try:
+    del stats['dual_thresh_mismatch_95width']
+    stats['thresh_mismatch'] = stats.pop('dual_thresh_mismatch_median').abs().apply(np.max)
+except KeyError:
+    pass
 #(stats/stats.max()).nsmallest(10, 'rms').plot.bar()
 
 fig = plt.figure()
@@ -95,7 +98,6 @@ top = (stats).nsmallest(10, 'rms')
 subplot = (top/top.max()).plot.bar(ax=ax1)
 text = [(col, '{:.2f}'.format(top[col].max())) for col in top]
 text = list(map(list, zip(*text))) #Transpose
-embed()
 table = ax2.table(cellText=text, cellLoc='center')
 table.auto_set_font_size(False)
 table.scale(1, 1.5)
@@ -104,3 +106,4 @@ ax2.axis('tight')
 ax2.axis('off')
 #(np.log10(stats/stats.max())).loc[stats.sum(axis='columns').nsmallest(10).index].plot.bar()
 plt.show()
+embed()
