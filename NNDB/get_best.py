c68f26e60eb40bef4a4a093ce923cb1e01f04cc7
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2017-12-18 17:21:12 +0100

    Minial get_best handeling multiple feature_names

--- NNDB/get_best.py
+++ NNDB/get_best.py
@@ -4,7 +4,7 @@ import gc
 import numpy as np
 import pandas as pd
 from itertools import chain
-from model import Network, NetworkJSON, NetworkMetadata, Hyperparameters, Postprocessing
+from model import Network, NetworkJSON, NetworkMetadata, Hyperparameters
 from peewee import Param
 import os
 import sys
@@ -18,25 +18,31 @@ target_to_fancy = {'efeETG_GB': 'Electron ETG Heat Flux',
                    'efeTEM_GB': 'Electron TEM Heat Flux',
                    'efiITG_GB': 'Ion ITG Heat Flux',
                    'efiTEM_GB': 'Ion TEM Heat Flux'}
-feature_names = ['An','Ate','Ati','Ti_Te','qx','smag','x']
+feature_names = ['An', 'Ate', 'Ati', 'Ti_Te', 'qx', 'smag', 'x']
+feature_names2 = ['Ati', 'Ate', 'An', 'qx', 'smag', 'x', 'Ti_Te']
 query = (Network.select(Network.target_names).distinct().tuples())
-df = pd.DataFrame(columns=['target_names', 'id','rms'])
+#df = pd.DataFrame(columns=['target_names', 'id','rms'])
+results = pd.DataFrame()
 for ii, query_res in enumerate(query):
     target_names = query_res[0]
     subquery = (Network.select(Network.id, NetworkMetadata.rms_validation)
                 .where(Network.target_names == Param(target_names))
-                .where(Network.feature_names == Param(feature_names))
+                .where((Network.feature_names == Param(feature_names))
+                       | (Network.feature_names == Param(feature_names2))
+                       )
                 .join(NetworkMetadata)
                 .order_by(NetworkMetadata.rms_validation)
-                .limit(1)
                 .tuples())
-    df.loc[ii] = list(chain([target_names], subquery.get()))
-df['id'] = df['id'].astype('int64')
+    df = pd.DataFrame(list(subquery), columns=['id','rms'])
+    index = pd.MultiIndex.from_product([[tuple(target_names)], df.index])
+    df.index = index
+    #df.loc[ii] = list(chain([target_names], subquery.get()))
+    results = pd.concat([results, df])
+results['id'] = results['id'].astype('int64')
 
-print(df)
-for row in df.iterrows():
-    df.set_value(row[0], 'target_names', target_to_fancy[row[1]['target_names']])
-df = df[['target_names', 'l2_norm', 'rms', 'rms_rel']]
-df.columns = ['Training Target', 'L_2 norm', 'RMS error [GB]', 'RMS error [%]']
-print(df.to_latex(index=False))
+#for row in results.iterrows():
+#    results.set_value(row[0], 'target_names', target_to_fancy[row[1]['target_names']])
+#results = results[['target_names', 'l2_norm', 'rms', 'rms_rel']]
+#results.columns = ['Training Target', 'L_2 norm', 'RMS error [GB]', 'RMS error [%]']
+#print(results.to_latex(index=False))
 embed()
