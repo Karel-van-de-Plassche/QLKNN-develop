22305e6448313cc7f5ea8685a58a58d2add13020
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2018-03-13 13:28:20 +0100

    Analysing of names now more robust. Added 'is growth?'

--- qlknn/misc/analyse_names.py
+++ qlknn/misc/analyse_names.py
@@ -10,7 +10,7 @@ momentum_vars = [u'vf']
 def split_parts(name):
     splitted = re.compile('((?:.{2})(?:.)(?:|ITG|ETG|TEM)_(?:GB|SI|cm))').split(name)
     if splitted[0] != '' or splitted[-1] != '':
-        raise Exception('Split {!s} in an unexpected way: {!s}'.format(name, splitted))
+        raise ValueError('Split {!s} in an unexpected way: {!s}'.format(name, splitted))
     del splitted[0], splitted[-1]
     return splitted
 
@@ -21,24 +21,38 @@ def extract_operations(splitted):
     return splitted[slice(1, len(splitted) - 1, 2)]
 
 def is_pure(name):
-    return len(split_parts(name)) == 1
+    try:
+        pure = len(split_parts(name)) == 1
+    except ValueError:
+        pure = False
+    return pure
 
 def is_flux(name):
-    bool = True
-    for part_name in split_parts(name):
-        bool &= split_name(name)[0] in heat_vars + particle_vars + momentum_vars
-    return bool
+    flux = True
+    try:
+        for part_name in split_parts(name):
+            flux &= split_name(name)[0] in heat_vars + particle_vars + momentum_vars
+    except ValueError:
+        flux = False
+    return flux
 
 def is_pure_flux(name):
-    flux = split_name(name)[0] in heat_vars + particle_vars + momentum_vars
-    return is_pure(name) and flux
+    try:
+        flux = split_name(name)[0] in heat_vars + particle_vars + momentum_vars
+        pure_flux = is_pure(name) and flux
+    except ValueError:
+        pure_flux = False
+    return pure_flux
 
 def split_name(name):
     splitted = re.compile('(.{2})(.)(|ITG|ETG|TEM)_(GB|SI|cm)').split(name)
     if splitted[0] != '' or splitted[-1] != '':
-        raise Exception('Split {!s} in an unexpected way: {!s}'.format(name, splitted))
+        raise ValueError('Split {!s} in an unexpected way: {!s}'.format(name, splitted))
     del splitted[0], splitted[-1]
     return splitted
+
+def is_growth(name):
+    return name in ['gam_leq_GB', 'gam_great_GB']
 
 if __name__ == '__main__':
     print(split_parts('efeITG_GB_div_efiITG_GB_plus_pfeITG_GB'))
