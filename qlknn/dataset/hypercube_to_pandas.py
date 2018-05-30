593b7e858d96cf1f1e6e897ba417f0585eb3c132
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2018-05-30 15:04:48 +0200

    More OOC! better gam_[leq|great] caching. Load vars in RAM before write

--- qlknn/dataset/hypercube_to_pandas.py
+++ qlknn/dataset/hypercube_to_pandas.py
@@ -18,7 +18,7 @@ except NameError:
 
 from qualikiz_tools.qualikiz_io.outputfiles import xarray_to_pandas
 
-GAM_LEQ_GB_TMP_PATH = 'gam_leq_GB.nc'
+GAM_LEQ_GB_TMP_PATH = 'gam_cache.nc'
 
 @profile
 def metadatize(ds):
@@ -50,12 +50,12 @@ def calculate_grow_vars(ds):
     gam = gam.max('numsols')
     gam_leq = gam.isel(kthetarhos=slice(None, bound_idx))
     gam_leq = gam_leq.max('kthetarhos')
+    gam_leq.name = 'gam_leq_GB'
     gam_great = gam.isel(kthetarhos=slice(bound_idx + 1, None))
     gam_great = gam_great.max('kthetarhos')
+    gam_great.name = 'gam_great_GB'
 
-    ds['gam_great_GB'] = gam_great
-    ds['gam_leq_GB'] = gam_leq
-    return ds
+    return gam_leq, gam_great
 
 @profile
 def determine_stability(ds):
@@ -68,23 +68,10 @@ def determine_stability(ds):
     ome = ome.isel(kthetarhos=slice(None, bound_idx))
 
     ion_unstable = (gam_leq != 0)
-    embed()
-    ds['TEM'] = ((ion_unstable & (ome > 0)).astype('bool').any(dim='numsols')).any(dim='kthetarhos')
-    ds['ITG'] = ((ion_unstable & (ome < 0)).astype('bool').any(dim='numsols')).any(dim='kthetarhos')
+    ds['TEM'] = ion_unstable & (ome > 0).any(dim=['kthetarhos', 'numsols'])
+    ds['ITG'] = ion_unstable & (ome < 0).any(dim=['kthetarhos', 'numsols'])
     return ds
 
-def prep_totflux(ds):
-    """ Prepare variables in 'totflux' dataset for NN training """
-    ds = absambi(ds)
-
-    ds = metadatize(ds)
-
-    #ds = calculate_grow_vars(ds)
-
-    ds = determine_stability(ds)
-
-    ds = ds.drop(['gam_GB', 'ome_GB'])
-    return ds
 
 @profile
 def sum_pf(df=None, vt=None, vr=0, vc=None, An=None):
@@ -112,12 +99,6 @@ def calculate_particle_sepfluxes(ds):
         ds[pf.name] = pf
     return ds
 
-@profile
-def prep_sepflux(ds):
-    """ Prepare variables in 'sepflux' dataset for NN training """
-    ds = metadatize(ds)
-    ds = calculate_particle_sepfluxes(ds)
-    return ds
 
 @profile
 def remove_rotation(ds):
@@ -137,17 +118,120 @@ def load_megarun1_ds(rootdir='.'):
     ds.close()
 
     # Re-open dataset with on-disk chunksizes
+    chunks = dict(zip(dims, chunk_sizes))
     ds_kwargs = {
-        'chunks': dict(zip(dims, chunk_sizes)),
+        'chunks': chunks,
         #'cache': True
         #'lock': lock
-              }
+    }
     ds = xr.open_dataset(os.path.join(rootdir, 'Zeffcombo.nc.1'),
                          **ds_kwargs)
     ds_sep = xr.open_dataset(os.path.join(rootdir, 'Zeffcombo.sep.nc.1'),
                              **ds_kwargs)
     ds_tot = ds.merge(ds_sep.data_vars)
-    return ds_tot, ds_kwargs
+    return ds_tot, ds_kwargs, chunks
+
+def get_dims_chunks(var):
+    if var.chunks is not None:
+        if isinstance(var.chunks, dict):
+            # xarray-style
+            sizes = var.chunks
+            chunksizes = [sizes[dim][0] if sizes[dim][:-1] == sizes[dim][1:] else None
+                          for dim in var.dims]
+        if isinstance(var.chunks, tuple):
+            # dask-style
+            chunksizes = [sizes[0] if sizes[1:] == sizes[:-1] else None for sizes in var.chunks]
+        if None in chunksizes:
+            raise Exception('Unequal size for one of the chunks in {!s}'.format(var.chunks.items()))
+    else:
+        raise NotImplementedError('Getting chunks of {!s}'.format(var))
+    return chunksizes
+
+@profile
+def merge_gam_leq_great(ds, ds_kwargs=None, rootdir='.', use_disk_cache=False, starttime=None):
+    if ds_kwargs is None:
+        ds_kwargs = {}
+
+    if starttime is None:
+        starttime = time.time()
+
+    gam_cache_dir = os.path.join(rootdir, GAM_LEQ_GB_TMP_PATH)
+    if not use_disk_cache:
+        gam_leq, gam_great = calculate_grow_vars(ds)
+        chunksizes = get_dims_chunks(gam_leq)
+
+        # Perserve chunks/encoding from gam_GB
+        import copy
+        #encoding = copy.deepcopy(ds['gam_GB'].encoding)
+        encoding = {}
+        #for key in ['original_shape', 'source', 'coordinates']:
+        #    encoding.pop(key)
+        for key in ['zlib', 'shuffle', 'complevel', 'dtype', ]:
+            encoding[key] = ds['gam_GB'].encoding[key]
+        encoding['chunksizes'] = chunksizes
+
+        gam_leq.load()
+        gam_leq.to_netcdf(gam_cache_dir, encoding={gam_leq.name: encoding})
+        gam_great.load()
+        gam_great.to_netcdf(gam_cache_dir, encoding={gam_great.name: encoding}, mode='a')
+
+    chunks = ds_kwargs.pop('chunks')
+    chunks.pop('kthetarhos')
+    chunks.pop('numsols')
+    ds_gam = xr.open_dataset(os.path.join(rootdir, GAM_LEQ_GB_TMP_PATH),
+                             chunks=chunks,
+                             **ds_kwargs)
+    ds = ds.merge(ds_gam.data_vars)
+
+    return ds
+
+def save_chunked_var():
+    pass
+
+@profile
+def compute_and_save(ds, new_ds_path, chunks=None):
+    new_ds = xr.Dataset()
+    for coord in ds.coords:
+        new_ds.coords[coord] = ds[coord]
+    new_ds.to_netcdf(new_ds_path)
+    print('Coords saved after', time.time() - starttime)
+    print(chunks)
+
+    data_vars = list(ds.data_vars)
+    calced_dims = ['gam_leq_GB', 'gam_great_GB', 'TEM', 'ITG', 'absambi']
+    for spec, mode in product(['e', 'i'], ['ITG', 'TEM']):
+        fluxes = ['df', 'vt', 'vc']
+        if spec == 'i':
+            fluxes.append('vr')
+        for flux in fluxes:
+            calced_dims.append(flux + spec + mode + '_GB')
+
+    for calced_dim in reversed(calced_dims):
+        if calced_dim in ds:
+            data_vars.insert(0, data_vars.pop(data_vars.index(calced_dim)))
+
+    for varname in data_vars:
+        print(varname)
+        #if varname not in ['gam_leq_GB', 'gam_great_GB', 'efiITG_GB', 'TEM', 'vtiITG_GB']:
+        ##    continue
+        #var = ds[varname].load()
+        var = ds[varname]
+        #del var.encoding['chunksizes']
+        #var = var.chunk({name: size for name, size in chunks.items() if name in ds[varname].dims})
+        #if chunks  is not None:
+        #    var.encoding['chunksizes'] = [chunks[dim] for dim in var.dims]
+        #var.encoding['chunksizes'] = [chunks[dim] for dim in var.dims]
+        #var = var.compute()
+        #var.load()
+        #var.encoding['chunksizes'] = [chunks[dim] for dim in var.dims]
+        #var = var.chunk({name: size for name, size in chunks.items() if name in ds[varname].dims})
+        #print(var.__class__)
+        #print(var.encoding)
+        #print(var.chunks)
+        var.to_netcdf(new_ds_path, 'a', encoding={varname: {'chunksizes':  [chunks[dim] for dim in var.dims], 'zlib': True}})
+        #del var
+        #gc.collect()
+        print(varname + ' saved after', time.time() - starttime)
 
 @profile
 def prep_megarun1_ds(starttime=None):
@@ -157,26 +241,23 @@ def prep_megarun1_ds(starttime=None):
     client = Client(processes=False)
     #client = Client()
     rootdir = '../../../qlk_data'
-    ds, ds_kwargs = load_megarun1_ds(rootdir)
+    ds, ds_kwargs, chunks = load_megarun1_ds(rootdir)
     print('Datasets merged after', time.time() - starttime)
-    use_cached_gam_leq = True
-    if not use_cached_gam_leq:
-        ds = calculate_grow_vars(ds)
-        ds_leq = ds['gam_leq_GB'].to_dataset()
-        ds_leq = ds_leq.merge(ds.coords) # Save all coords
-        ds_leq = profile(ds_leq.compute()) # Pre-compute, way faster as this fits in RAM!
-        ds_leq.to_netcdf(os.path.join(rootdir, GAM_LEQ_GB_TMP_PATH))
-    ds_leq = xr.open_dataset(os.path.join(rootdir, GAM_LEQ_GB_TMP_PATH),
-                             **ds_kwargs)
-    ds = ds.merge(ds, ds.data_vars)
-    print('gam_leq_GB written after', time.time() - starttime)
+    use_disk_cache = True
+    #use_cached_gam = False
+    ds = merge_gam_leq_great(ds, ds_kwargs=ds_kwargs, rootdir=rootdir, use_disk_cache=use_disk_cache, starttime=starttime)
+    print('gam_[leq,great]_GB written after', time.time() - starttime)
 
-    ds = prep_totflux(ds)
-    print('Computed totflux after', time.time() - starttime)
-    ds = prep_sepflux(ds)
-    print('Computed sepflux after', time.time() - starttime)
+    ds = absambi(ds)
+    print('Computed absambi after', time.time() - starttime)
 
+    ds = calculate_particle_sepfluxes(ds)
+    print('Computed particle sepfluxes after', time.time() - starttime)
+    ds = ds.drop(['gam_GB', 'ome_GB'])
     ds = remove_rotation(ds)
+    ds = metadatize(ds)
+    ds.attrs['normni'] = ds.drop('normni')
+
     #ds.to_netcdf('Zeffcombo.combo.nc', format='NETCDF4', engine='netcdf4')
 
     # Remove all but first ion
@@ -184,9 +265,10 @@ def prep_megarun1_ds(starttime=None):
 
     print('starting after {:.0f}s'.format(time.time() - starttime))
     with Profiler() as prof, ResourceProfiler(dt=0.25) as rprof, CacheProfiler() as cprof:
-        ds.to_netcdf('Zeffcombo.combo.nions0.nc', format='NETCDF4', engine='netcdf4')
+        new_ds_path = os.path.join(rootdir, 'Zeffcombo_prepared.nc.1')
+        compute_and_save(ds, new_ds_path, chunks=chunks)
     print('prep_megarun1 done after {:.0f}s'.format(time.time() - starttime))
-    visualize([prof, rprof, cprof], file_path='profile_' + backend + '.html')
+    visualize([prof, rprof, cprof], file_path='profile_prep.html')
     client.close()
     return ds
 
@@ -207,6 +289,8 @@ def save_trainframe(df, constants):
 if __name__ == '__main__':
     starttime = time.time()
     ds_tot = prep_megarun1_ds(starttime=starttime)
+    print('Done')
+    exit()
     scan_dims = tuple(dim for dim in ds_tot.dims if dim != 'kthetarhos' and dim != 'nions' and dim != 'numsols')
 
     # Convert to pandas
