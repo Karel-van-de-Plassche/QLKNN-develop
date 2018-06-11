a70afd7553c6635a19893710bb38a300e6755254
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2018-06-11 17:13:09 +0200

    Python3 compat

--- qlknn/misc/tools.py
+++ qlknn/misc/tools.py
@@ -32,17 +32,21 @@ def ordered_dict_prepend(dct, key, value, dict_setitem=dict.__setitem__):
     https://stackoverflow.com/a/16664932/3613853
 
     """
-    root = dct._OrderedDict__root
-    first = root[1]
+    if hasattr(dct, 'move_to_end'):
+        dct[key] = value
+        dct.move_to_end(key, last=False)
+    else: # Before Python3.2
+        root = dct._OrderedDict__root
+        first = root[1]
 
-    if key in dct:
-        link = dct._OrderedDict__map[key]
-        link_prev, link_next, _ = link
-        link_prev[1] = link_next
-        link_next[0] = link_prev
-        link[0] = root
-        link[1] = first
-        root[1] = first[0] = link
-    else:
-        root[1] = first[0] = dct._OrderedDict__map[key] = [root, first, key]
-        dict_setitem(dct, key, value)
+        if key in dct:
+            link = dct._OrderedDict__map[key]
+            link_prev, link_next, _ = link
+            link_prev[1] = link_next
+            link_next[0] = link_prev
+            link[0] = root
+            link[1] = first
+            root[1] = first[0] = link
+        else:
+            root[1] = first[0] = dct._OrderedDict__map[key] = [root, first, key]
+            dict_setitem(dct, key, value)
